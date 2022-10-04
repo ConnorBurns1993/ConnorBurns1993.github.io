d9c14b925afc5a6bca213d4f03615edc905e8c33
Connor Burns <cburns1993@gmail.com>
2022-10-04 13:19:31 -0700

    test

--- bundler/webpack.prod.js
+++ bundler/webpack.prod.js
@@ -2,15 +2,7 @@ const { merge } = require("webpack-merge");
 const commonConfiguration = require("./webpack.common.js");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
-module.exports = merge(
-  commonConfiguration,
-  {
-    output: {
-      publicPath: "/ConnorBurns1993.github.io/",
-    },
-  },
-  {
-    mode: "production",
-    plugins: [new CleanWebpackPlugin()],
-  }
-);
+module.exports = merge(commonConfiguration, {
+  mode: "production",
+  plugins: [new CleanWebpackPlugin()],
+});
